ace83176d55d8971cd75a656e8f8018747fc5b88
Jacob Squier <jacob.squier@gmail.com>
2020-08-25 00:34:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,4 @@
 class Dog 
   
-  def initialize(breed) 
-    @breed = breed
-  end 
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end 
-end 
-
-slime = Dog.new("Mutt")
+  def initialize(name, breed)
+    
